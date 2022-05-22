414947cb8c305d454956fe0d1e1161682457f9d6
amasparera <amasparera@gmail.com>
2022-05-22 11:25:18 +0700

    structur

---
+++ android/app/src/main/kotlin/com/example/openart_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.openart_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:openart_app/injection.dart' as injection;
+
+void main() {
+  injection.init();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: Scaffold());
+  }
+}
